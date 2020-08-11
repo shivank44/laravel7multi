5edfec7b603879152a2a3f5a7cf01d843a793219
Shivank Srivastava <shivank@atdevelops.com>
2020-08-11 11:04:55 +0530

    Add UserTable Seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => 'admin@123',
+            'role_id' => '1'
+        ]);
+    }
+}
